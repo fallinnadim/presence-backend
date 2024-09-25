0b6b322ff7874de5fcc92927f7ac1836d8d349f5
Ahmad Zaky Nadimsyah <nadimwkwk@gmail.com>
2024-09-25 21:48:04 +0700

    initial commit

---
+++ src/main/java/com/fallinnadim/presence_app/PresenceAppApplication.java
@@ -0,0 +1,13 @@
+package com.fallinnadim.presence_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresenceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fallinnadim/presence_app/PresenceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fallinnadim.presence_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
